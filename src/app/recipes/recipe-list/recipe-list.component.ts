668cc9e43e595a6b398c844ca9b95a497a3d2931
Elsawaf001 <71516525+Elsawaf001@users.noreply.github.com>
2023-01-05 14:08:10 +0200

    6 files modified, 1 added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'a test recipe',
+      'A test Recipe',
+      'https://www.tasteofhome.com/wp-content/uploads/2020/01/Easy-Pad-Thai_EXPS_FT20_249632_F_0109_1.jpg?resize=768,768'
+    ),
+    new Recipe(
+      'a test recipe',
+      'A test Recipe',
+      'https://www.tasteofhome.com/wp-content/uploads/2020/01/Easy-Pad-Thai_EXPS_FT20_249632_F_0109_1.jpg?resize=768,768'
+    ),
+    new Recipe(
+      'a test recipe',
+      'A test Recipe',
+      'https://www.tasteofhome.com/wp-content/uploads/2020/01/Easy-Pad-Thai_EXPS_FT20_249632_F_0109_1.jpg?resize=768,768'
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,13 @@
+export class Recipe {
+  public name: string;
+  public descreption :string;
+  public imagePath: string;
+
+  constructor(name:string , descreption:string , imagePath:string ){
+this.descreption=descreption;
+this.imagePath = imagePath;
+this.name=name;
+
+
+  }
+}
